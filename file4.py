635a1caf70fd53f47dfb3b986b32e0cccc82cbc8
max44910 <s.l.quinn@hotmail.com>
2025-09-18 19:41:08 +0100

    Add files via upload

---
+++ file4.py
@@ -0,0 +1,24 @@
+def decimal_to_binary_converter():
+    """
+
+    """
+    
+    try:
+        decimal_num = int(input("Enter a decimal number: "))
+    except ValueError:
+        print("Invalid input. Please enter a whole number.")
+        return
+
+    
+    if decimal_num == 0:
+        print("The binary representation of 0 is 0.")
+        return
+
+
+    binary_num_string = ""
+
+    temp_num = decimal_num
+
+    while temp_num > 0:
+        
+        remainder = temp_num % 2
